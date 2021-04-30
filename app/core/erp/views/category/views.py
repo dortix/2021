2aa22d16a3fcd332c9ff945d32761db37ebac55c
Danilo ortiz <danilo_ort@hotmail.com>
2021-04-29 21:57:58 -0500

    agregando forms

--- app/core/erp/views/category/views.py
+++ app/core/erp/views/category/views.py
@@ -1,6 +1,13 @@
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+
 from core.erp.models import Category
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
+from django.utils.decorators import method_decorator
+
+from core.forms import CategoryForm
 
 
 def category_list(request):
@@ -15,7 +22,30 @@ class CategorylistView(ListView):
     model = Category
     template_name = 'category/list.html'
 
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def post(self, request, *arg, **kwargs):
+        data = {}
+        try:
+            data = Category.objects.get(pk=request.POST['id']).toJson()
+        except Exception as e:
+            data['error'] = str(e)
+        return JsonResponse(data)
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Listado de Categorias'
         return context
+
+
+class CategoryCreateView(CreateView):
+    model = Category
+    form_class = CategoryForm
+    template_name = 'category/create.html'
+
+    def get_context_data(self, **kwargs):
+         context = super().get_context_data(**kwargs)
+         context['title'] = 'Agregar Categoria'
+         return context
---
+++ app/core/forms.py
@@ -0,0 +1,8 @@
+from core.erp.models import Category
+
+from django.forms import ModelForm
+
+class CategoryForm(ModelForm):
+    class Meta:
+        model = Category
+        fields = '__all__'
